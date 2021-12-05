d15a842466bb49d7874b991dc131a4ffd605ca80
Carlos Alcala <alcala21@gmail.com>
2021-12-04 22:11:19 -0600

    Stage 2/7. University Admission Procedure

--- university.py
+++ university.py
@@ -1,3 +1,7 @@
-# write your code here
-print(f"{sum([int(input()) for _ in range(3)])/ 3}")
-print("Congratulations, you are accepted!")
+n = 3
+mean_val = sum([int(input()) for _ in range(n)]) / n
+print(mean_val)
+if mean_val >= 60.0:
+    print("Congratulations, you are accepted!")
+else:
+    print("We regret to inform you that we will not be able to offer you admission.")
