5ba6c236f7fe09f62c33be82822b28177efdee53
Carlos Alcala <alcala21@gmail.com>
2021-12-05 18:21:28 -0600

    Stage 5/7. University Admission Procedure

--- university.py
+++ university.py
@@ -7,21 +7,24 @@ fname = "applicants.txt"
 
 
 class Student:
-    def __init__(self, name, last, gpa, first, second, third):
+    def __init__(self, name, last, phy, chem, math, cs, first, second, third):
         self.name = name
         self.last = last
-        self.gpa = float(gpa)
         self.options = {"first": first, "second": second, "third": third}
+        self.scores = {"Physics": float(phy), "Chemistry": float(chem),
+                       "Mathematics": float(math), "Engineering": float(cs), "Biotech": float(chem)}
         self.full_name = f"{self.name} {self.last}"
 
+
 class Department:
-    def __init__(self, name, n):
+    def __init__(self, name, _n):
         self.name = name
-        self.needed = n
+        self.needed = _n
         self.accepted = []
 
     def admit(self, candidates):
         rejected = []
+        candidates = self.sort(candidates)
         if len(candidates) <= self.needed:
             self.accepted += candidates
             self.needed -= len(candidates)
@@ -30,34 +33,29 @@ class Department:
                 self.accepted += [candidates[i]]
             rejected = candidates[self.needed:]
             self.needed = 0
+        self.accepted = self.sort(self.accepted)
         return rejected
 
-    def sort(self):
-        self.accepted = sorted(self.accepted, key=lambda x: (-x.gpa, x.name, x.last))
+    def sort(self, _students):
+        return sorted(_students, key=lambda x: (-x.scores[self.name], x.name, x.last))
 
 
 departments = {x: Department(x, n) for x in department_names}
-
-
 applicants = []
 with open(fname, 'r') as f:
     for line in f:
         applicants.append(Student(*line.split()))
 
 for option in ['first', 'second', 'third']:
-    applicants = sorted(applicants, key=lambda x: (-x.gpa, x.name, x.last))
     dep_candidates = defaultdict(list)
     for app in applicants:
         dep_candidates[app.options[option]].append(app)
-    rejected = []
-    for dep in department_names:
-        rejected += departments[dep].admit(dep_candidates[dep])
-    applicants = rejected[:]
+    applicants = sum([departments[dep].admit(dep_candidates[dep])
+                      for dep in department_names], [])
 
 for dep in department_names:
     print(departments[dep].name)
-    departments[dep].sort()
     for applicant in departments[dep].accepted:
-        print(f"{applicant.full_name} {applicant.gpa}")
+        print(f"{applicant.full_name} {applicant.scores[dep]}")
     print()
 
